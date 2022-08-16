4f65313d0958d91a08d2831ff538058c571527ec
Vladimir <101829315+VladimirSkills@users.noreply.github.com>
2022-08-16 19:20:49 +0400

    Delete auth_page.py

--- pages/auth_page.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-
-
-
