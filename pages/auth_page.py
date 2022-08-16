05aec516d1ddbcf078168c21a31c80a71c11e29e
VladimirSkills <pob_1@mail.ru>
2022-08-16 19:17:17 +0400

    Add file example

---
+++ pages/auth_page.py
@@ -0,0 +1,8 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+
